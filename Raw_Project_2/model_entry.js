ff8d9ea426b96c72e0aaab70ad2bee6ab5ef3a2b
Willie Gadson <dontagadson92@gmail.com>
2020-05-12 11:33:09 -0400

    remmendations added to db

---
+++ Raw_Project_2/model_entry.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const sequelize = require('./connection.js');
+
+// create model
+const Movie = sequelize.define("movie", {
+  //movie will turn into movies when searching db
+  movieTitle: Sequelize.STRING,
+  recommendation: Sequelize.STRING,
+  userName: Sequelize.STRING,
+//   rating: Sequelize.INTEGER,
+});
+
+Movie.sync();
+
+module.exports = Movie;
---
+++ Raw_Project_2/movies_DB;.sql
@@ -0,0 +1,15 @@
+-- Drop database if exists movies_DB;
+-- CREATE DATABASE movies_DB;
+-- USE movies_DB;
+
+-- CREATE TABLE moviesTable (
+--   id Int( 11 ) AUTO_INCREMENT NOT NULL,
+--   movieTitle VARCHAR( 255) NOT NULL,
+--   userName VARCHAR( 255) NOT NULL,
+--   recommendation VARCHAR( 255 ) NOT NULL,
+--   rating DATETIME NOT NULL,
+
+--   PRIMARY KEY (id) 
+-- );
+
+-- select*from moviesTable
