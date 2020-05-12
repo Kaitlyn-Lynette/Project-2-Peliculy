fd75f9cfd44f89b7537a454669393133e86cb65d
Kaitlyn Rodriguez <kaitlynrodriguez@Kaitlyns-MacBook-Air.local>
2020-05-12 12:27:09 -0400

    Clean files with Willys JS

--- Raw_Project_2/api_route.js
+++ Raw_Project_2/api_route.js
@@ -1,28 +1,30 @@
 
 // // Dependencies
 // // =============================================================
-const Entry = require("./model_entry");
-//This is the 
+const Entry = require('./model_entry');
+//This is the
 
 // // Routes
 // // =============================================================
 module.exports = function(app) {
   // If a user sends data to add a new recommendation entry...
-  app.post("/api/new", function(req, res) {
-    console.log(req)
-    console.log("hit the server")
+  app.post('/api/new', function(req, res) {
+    console.log(req);
+    console.log('hit the server');
     var entryRec = req.body;
-    console.log("entryREc", entryRec);
+    console.log('entryREc', entryRec);
     Entry.create({
-        // creates table in DB
-        movieTitle: entryRec.movieTitle,
-        userName: entryRec.userName,
-        reommedation: entryRec.reommedation,
-  }).then(function(result){
-    console.log(result)
-    res.json(result)
-    res.end();}
-    
+      // creates table in DB
+      movieTitle: entryRec.movieTitle,
+      userName: entryRec.userName,
+      reommedation: entryRec.reommedation,
+    }).then(function(result){
+      console.log(result);
+      res.json(result);
+      res.end();
+    }
 
-  );
-})}
+
+    );
+  });
+};
