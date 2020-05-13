9bad8a5a95613fd72bfd11cffc0131b8033aaba1
Kaitlyn Rodriguez <kaitlynrodriguez@Kaitlyns-MacBook-Air.local>
2020-05-13 13:51:00 -0400

    Adding the jquery code

--- routes/rec-routes.js
+++ routes/rec-routes.js
@@ -6,8 +6,17 @@ var db = require('../models');
 module.exports = function(app) {
 
 
+//GET route for getting all the recommendations written
+
+  app.get('/api/recommendation',function(req,res) {
+    db.Recommendation.findAll({})
+      .then(function(dbRecommendation){
+        res.json('This is a new rec',dbRecommendation);
+      });
+  });
+
   // POST route for saving a new recommendation
-  app.post('/api/recommendations', function(req, res) {
+  app.post('/api/recommendation', function(req, res) {
     db.Recommendations.create(req.body).then(function(dbRecommendation) {
       res.json(dbRecommendation);
     });
