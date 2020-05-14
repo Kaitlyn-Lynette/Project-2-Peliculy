be2689becd305f1b4409a6ef12d0d9d6d224f9cf
Willie Gadson <dontagadson92@gmail.com>
2020-05-13 22:11:03 -0400

    added recomment

---
+++ routes/newsfeed.js
@@ -0,0 +1,7 @@
+app.get("/api/todos", function(req, res) {
+    // findAll returns all entries for a table when used with no options
+    db.Todo.findAll({}).then(function(dbTodo) {
+      // We have access to the todos as an argument inside of the callback function
+      res.json(dbTodo);
+    });
+  });
