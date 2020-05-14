cd6ec9c5dcdf31230900e7679925e3062abcc063
Kaitlyn Rodriguez <kaitlynrodriguez@Kaitlyns-MacBook-Air.local>
2020-05-14 11:04:37 -0400

    removed model

--- models/recommend.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var Recommend = sequelize.define('Recommend', {
-    movieTitle: DataTypes.STRING,
-    recommendation: DataTypes.STRING,
-    userName: DataTypes.STRING,
-  });
-  return Recommend;
-};
