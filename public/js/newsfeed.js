8d47afef5bc73e79568b4e438faf3ca0aaeb11e3
Kaitlyn Rodriguez <kaitlynrodriguez@Kaitlyns-MacBook-Air.local>
2020-05-14 10:36:04 -0400

    last night edits

--- public/js/newsfeed.js
+++ public/js/newsfeed.js
@@ -1,22 +1,19 @@
-$(document).ready(function(){
-  var newsfeedContainer = $('.newsfeed-container');
-  var recs;
 
-  //This will grabs posts from the database and update the view
-  $.get('/api/recommendations', function(data){
-    console.log(data);
 
-    const recommend = [
-        {
-            movieTitle: "Jaws",
-            recommendation: "It was scary",
-            userName: "Ben"
-        }
-    ];
-        res.json(recommendation);
-        $("#movie-title").text(data.name);
-        $("#recommendation").text(data.role);
-        $("#username").text(data.userName);
-    });
+//This will grabs posts from the database and update the view
+$.get('/api/recommend', function(data){
+  console.log(data);
+
+  //   const recommend = [
+  //     {
+  //       movieTitle: 'Jaws',
+  //       recommendation: 'It was scary',
+  //       userName: 'Ben'
+  //     }
+  //   ];
+  $('#movie-title').text(data.name);
+  $('#recommendation').text(data.role);
+  $('#username').text(data.userName);
+});
 
 module.export
