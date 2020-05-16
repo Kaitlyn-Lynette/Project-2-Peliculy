56a4fadd4d9a7fdae521514badf6274351ae12df
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2020-05-16 12:43:41 -0400

    Pushing up test.js

--- test.js
+++ test.js
@@ -1,29 +1,3 @@
-// import Axios from "axios";
-
-
-// const status = axios.get('http://localhost:4044/newsfeed');
-
-// console.log (status);
-
-
-// axios.get('http://localhost:4044/newsfeed')
-//   .then(response => console.log(response))
-//   .then(data => console.log(data));
-
-// async function asyncFunc() {
-// // fetch data from a url endpoint
-//     try{
-//         const data = await axios.get('http://localhost:4044/recommend');
-//         console.log(data);
-//         return data;
-//     }
-//     catch {
-//         console.log("Error");
-//     }
-// }
-// asyncFunc();
-
-
 const axios = require('axios');
 // Make a request for a user with a given ID
 axios.get('http://localhost:4044/newsfeed')
